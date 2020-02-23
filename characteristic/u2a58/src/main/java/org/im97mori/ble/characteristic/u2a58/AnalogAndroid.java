24dca62bcbc28f8bf7251f9459e6d00792ffc605
im97mori <github@im97mori.org>
2020-02-23 19:59:57 +0900

    change aios characteristic module

---
+++ characteristic/u2a58/src/main/java/org/im97mori/ble/characteristic/u2a58/AnalogAndroid.java
@@ -0,0 +1,89 @@
+package org.im97mori.ble.characteristic.u2a58;
+
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+import org.im97mori.ble.ByteArrayCreater;
+
+import static org.im97mori.ble.BLEConstants.CharacteristicUUID.ANALOG_CHARACTERISTIC;
+
+/**
+ * Analog (Characteristics UUID: 0x2A58)
+ */
+@SuppressWarnings({"WeakerAccess", "unused"})
+public class AnalogAndroid extends Analog implements Parcelable {
+
+    /**
+     * @see ByteArrayCreater
+     */
+    public static final ByteArrayCreater<AnalogAndroid> CREATOR = new ByteArrayCreater<AnalogAndroid>() {
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        @NonNull
+        public AnalogAndroid createFromParcel(@NonNull Parcel in) {
+            return new AnalogAndroid(in);
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        @NonNull
+        public AnalogAndroid[] newArray(int size) {
+            return new AnalogAndroid[size];
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @NonNull
+        public AnalogAndroid createFromByteArray(@NonNull byte[] values) {
+            BluetoothGattCharacteristic bluetoothGattCharacteristic = new BluetoothGattCharacteristic(ANALOG_CHARACTERISTIC, 0, 0);
+            bluetoothGattCharacteristic.setValue(values);
+            return new AnalogAndroid(bluetoothGattCharacteristic);
+        }
+
+    };
+
+    /**
+     * Constructor from {@link BluetoothGattCharacteristic}
+     *
+     * @param bluetoothGattCharacteristic Characteristics UUID: 0x2A58
+     */
+    public AnalogAndroid(@NonNull BluetoothGattCharacteristic bluetoothGattCharacteristic) {
+        super(bluetoothGattCharacteristic.getValue());
+    }
+
+    /**
+     * Constructor from {@link Parcel}
+     *
+     * @param in Parcel
+     */
+    @SuppressWarnings("ConstantConditions")
+    private AnalogAndroid(@NonNull Parcel in) {
+        super(in.createByteArray());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeByteArray(getBytes());
+    }
+
+}
---
+++ characteristic/u2a5a/src/main/java/org/im97mori/ble/characteristic/u2a5a/AggregateAndroid.java
@@ -0,0 +1,89 @@
+package org.im97mori.ble.characteristic.u2a5a;
+
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+import org.im97mori.ble.ByteArrayCreater;
+
+import static org.im97mori.ble.BLEConstants.CharacteristicUUID.AGGREGATE_CHARACTERISTIC;
+
+/**
+ * Aggregate (Characteristics UUID: 0x2A5A)
+ */
+@SuppressWarnings({"WeakerAccess", "unused"})
+public class AggregateAndroid extends Aggregate implements Parcelable {
+
+    /**
+     * @see ByteArrayCreater
+     */
+    public static final ByteArrayCreater<AggregateAndroid> CREATOR = new ByteArrayCreater<AggregateAndroid>() {
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        @NonNull
+        public AggregateAndroid createFromParcel(@NonNull Parcel in) {
+            return new AggregateAndroid(in);
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        @NonNull
+        public AggregateAndroid[] newArray(int size) {
+            return new AggregateAndroid[size];
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @NonNull
+        public AggregateAndroid createFromByteArray(@NonNull byte[] values) {
+            BluetoothGattCharacteristic bluetoothGattCharacteristic = new BluetoothGattCharacteristic(AGGREGATE_CHARACTERISTIC, 0, 0);
+            bluetoothGattCharacteristic.setValue(values);
+            return new AggregateAndroid(bluetoothGattCharacteristic);
+        }
+
+    };
+
+    /**
+     * Constructor from {@link BluetoothGattCharacteristic}
+     *
+     * @param bluetoothGattCharacteristic Characteristics UUID: 0x2A5A
+     */
+    public AggregateAndroid(@NonNull BluetoothGattCharacteristic bluetoothGattCharacteristic) {
+        super(bluetoothGattCharacteristic.getValue());
+    }
+
+    /**
+     * Constructor from {@link Parcel}
+     *
+     * @param in Parcel
+     */
+    @SuppressWarnings("ConstantConditions")
+    private AggregateAndroid(@NonNull Parcel in) {
+        super(in.createByteArray());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeByteArray(getBytes());
+    }
+
+}
