ce71269764393a0d137c10af8d283baa66dd9318
im97mori <github@im97mori.org>
2021-07-08 18:59:12 +0900

    update Count 24 characteristic

--- characteristic/u2aeb/src/main/java/org/im97mori/ble/characteristic/u2aeb/Count24Android.java
+++ characteristic/u2aeb/src/main/java/org/im97mori/ble/characteristic/u2aeb/Count24Android.java
@@ -13,22 +13,21 @@ import static org.im97mori.ble.constants.CharacteristicUUID.COUNT_24_CHARACTERIS
 /**
  * Count 24 (Characteristics UUID: 0x2AEB)
  */
-// TODO
 @SuppressWarnings({"WeakerAccess"})
-public class Count_24Android extends Count_24 implements Parcelable {
+public class Count24Android extends Count24 implements Parcelable {
 
     /**
      * @see ByteArrayCreater
      */
-    public static final ByteArrayCreater<Count_24Android> CREATOR = new ByteArrayCreater<Count_24Android>() {
+    public static final ByteArrayCreater<Count24Android> CREATOR = new ByteArrayCreater<Count24Android>() {
 
         /**
          * {@inheritDoc}
          */
         @Override
         @NonNull
-        public Count_24Android createFromParcel(@NonNull Parcel in) {
-            return new Count_24Android(in);
+        public Count24Android createFromParcel(@NonNull Parcel in) {
+            return new Count24Android(in);
         }
 
         /**
@@ -36,18 +35,18 @@ public class Count_24Android extends Count_24 implements Parcelable {
          */
         @Override
         @NonNull
-        public Count_24Android[] newArray(int size) {
-            return new Count_24Android[size];
+        public Count24Android[] newArray(int size) {
+            return new Count24Android[size];
         }
 
         /**
          * {@inheritDoc}
          */
         @NonNull
-        public Count_24Android createFromByteArray(@NonNull byte[] values) {
+        public Count24Android createFromByteArray(@NonNull byte[] values) {
             BluetoothGattCharacteristic bluetoothGattCharacteristic = new BluetoothGattCharacteristic(COUNT_24_CHARACTERISTIC, 0, 0);
             bluetoothGattCharacteristic.setValue(values);
-            return new Count_24Android(bluetoothGattCharacteristic);
+            return new Count24Android(bluetoothGattCharacteristic);
         }
 
     };
@@ -57,8 +56,17 @@ public class Count_24Android extends Count_24 implements Parcelable {
      *
      * @param bluetoothGattCharacteristic Characteristics UUID: 0x2AEB
      */
-    public Count_24Android(@NonNull BluetoothGattCharacteristic bluetoothGattCharacteristic) {
+    public Count24Android(@NonNull BluetoothGattCharacteristic bluetoothGattCharacteristic) {
         super(bluetoothGattCharacteristic.getValue());
+    }
+
+    /**
+     * Constructor from parameters
+     *
+     * @param count Count
+     */
+    public Count24Android(int count) {
+        super(count);
     }
 
     /**
@@ -66,7 +74,7 @@ public class Count_24Android extends Count_24 implements Parcelable {
      *
      * @param in Parcel
      */
-    private Count_24Android(@NonNull Parcel in) {
+    private Count24Android(@NonNull Parcel in) {
         //noinspection ConstantConditions
         super(in.createByteArray());
     }
