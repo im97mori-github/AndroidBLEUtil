40636e532d2cd4f6dc5439ba0bb4816b82e5e1bb
im97mori <github@im97mori.org>
2021-07-18 13:21:42 +0900

    update Time Second 16 characteristic

--- characteristic/u2b16/src/main/java/org/im97mori/ble/characteristic/u2b16/TimeSecond16Android.java
+++ characteristic/u2b16/src/main/java/org/im97mori/ble/characteristic/u2b16/TimeSecond16Android.java
@@ -13,22 +13,21 @@ import static org.im97mori.ble.constants.CharacteristicUUID.TIME_SECOND_16_CHARA
 /**
  * Time Second 16 (Characteristics UUID: 0x2B16)
  */
-// TODO
 @SuppressWarnings({"WeakerAccess"})
-public class TimeSecond_16Android extends TimeSecond_16 implements Parcelable {
+public class TimeSecond16Android extends TimeSecond16 implements Parcelable {
 
     /**
      * @see ByteArrayCreater
      */
-    public static final ByteArrayCreater<TimeSecond_16Android> CREATOR = new ByteArrayCreater<TimeSecond_16Android>() {
+    public static final ByteArrayCreater<TimeSecond16Android> CREATOR = new ByteArrayCreater<TimeSecond16Android>() {
 
         /**
          * {@inheritDoc}
          */
         @Override
         @NonNull
-        public TimeSecond_16Android createFromParcel(@NonNull Parcel in) {
-            return new TimeSecond_16Android(in);
+        public TimeSecond16Android createFromParcel(@NonNull Parcel in) {
+            return new TimeSecond16Android(in);
         }
 
         /**
@@ -36,18 +35,18 @@ public class TimeSecond_16Android extends TimeSecond_16 implements Parcelable {
          */
         @Override
         @NonNull
-        public TimeSecond_16Android[] newArray(int size) {
-            return new TimeSecond_16Android[size];
+        public TimeSecond16Android[] newArray(int size) {
+            return new TimeSecond16Android[size];
         }
 
         /**
          * {@inheritDoc}
          */
         @NonNull
-        public TimeSecond_16Android createFromByteArray(@NonNull byte[] values) {
+        public TimeSecond16Android createFromByteArray(@NonNull byte[] values) {
             BluetoothGattCharacteristic bluetoothGattCharacteristic = new BluetoothGattCharacteristic(TIME_SECOND_16_CHARACTERISTIC, 0, 0);
             bluetoothGattCharacteristic.setValue(values);
-            return new TimeSecond_16Android(bluetoothGattCharacteristic);
+            return new TimeSecond16Android(bluetoothGattCharacteristic);
         }
 
     };
@@ -57,16 +56,26 @@ public class TimeSecond_16Android extends TimeSecond_16 implements Parcelable {
      *
      * @param bluetoothGattCharacteristic Characteristics UUID: 0x2B16
      */
-    public TimeSecond_16Android(@NonNull BluetoothGattCharacteristic bluetoothGattCharacteristic) {
+    public TimeSecond16Android(@NonNull BluetoothGattCharacteristic bluetoothGattCharacteristic) {
         super(bluetoothGattCharacteristic.getValue());
     }
+
+    /**
+     * Constructor from parameters
+     *
+     * @param timeSecond16 Time Second 16
+     */
+    public TimeSecond16Android(int timeSecond16) {
+        super(timeSecond16);
+    }
+
 
     /**
      * Constructor from {@link Parcel}
      *
      * @param in Parcel
      */
-    private TimeSecond_16Android(@NonNull Parcel in) {
+    private TimeSecond16Android(@NonNull Parcel in) {
         //noinspection ConstantConditions
         super(in.createByteArray());
     }
