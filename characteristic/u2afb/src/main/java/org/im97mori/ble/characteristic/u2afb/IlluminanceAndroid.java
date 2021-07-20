cc8c66ada887f63e19025b2c84744c11c5b4c1a0
im97mori <github@im97mori.org>
2021-07-20 10:43:24 +0900

    update Illuminance characteristic

--- characteristic/u2afb/src/main/java/org/im97mori/ble/characteristic/u2afb/IlluminanceAndroid.java
+++ characteristic/u2afb/src/main/java/org/im97mori/ble/characteristic/u2afb/IlluminanceAndroid.java
@@ -13,7 +13,6 @@ import static org.im97mori.ble.constants.CharacteristicUUID.ILLUMINANCE_CHARACTE
 /**
  * Illuminance (Characteristics UUID: 0x2AFB)
  */
-// TODO
 @SuppressWarnings({"WeakerAccess"})
 public class IlluminanceAndroid extends Illuminance implements Parcelable {
 
@@ -59,6 +58,15 @@ public class IlluminanceAndroid extends Illuminance implements Parcelable {
      */
     public IlluminanceAndroid(@NonNull BluetoothGattCharacteristic bluetoothGattCharacteristic) {
         super(bluetoothGattCharacteristic.getValue());
+    }
+
+    /**
+     * Constructor from parameters
+     *
+     * @param illuminance Illuminance
+     */
+    public IlluminanceAndroid(int illuminance) {
+        super(illuminance);
     }
 
     /**
