a609b66c9b13a51935148d64db9c46dc64677a04
im97mori <github@im97mori.org>
2021-07-27 17:13:15 +0900

    update Time Hour 24 characteristic

--- characteristic/u2b14/src/main/java/org/im97mori/ble/characteristic/u2b14/TimeHour24Android.java
+++ characteristic/u2b14/src/main/java/org/im97mori/ble/characteristic/u2b14/TimeHour24Android.java
@@ -13,22 +13,21 @@ import static org.im97mori.ble.constants.CharacteristicUUID.TIME_HOUR_24_CHARACT
 /**
  * Time Hour 24 (Characteristics UUID: 0x2B14)
  */
-// TODO
 @SuppressWarnings({"WeakerAccess"})
-public class TimeHour_24Android extends TimeHour_24 implements Parcelable {
+public class TimeHour24Android extends TimeHour24 implements Parcelable {
 
     /**
      * @see ByteArrayCreater
      */
-    public static final ByteArrayCreater<TimeHour_24Android> CREATOR = new ByteArrayCreater<TimeHour_24Android>() {
+    public static final ByteArrayCreater<TimeHour24Android> CREATOR = new ByteArrayCreater<TimeHour24Android>() {
 
         /**
          * {@inheritDoc}
          */
         @Override
         @NonNull
-        public TimeHour_24Android createFromParcel(@NonNull Parcel in) {
-            return new TimeHour_24Android(in);
+        public TimeHour24Android createFromParcel(@NonNull Parcel in) {
+            return new TimeHour24Android(in);
         }
 
         /**
@@ -36,18 +35,18 @@ public class TimeHour_24Android extends TimeHour_24 implements Parcelable {
          */
         @Override
         @NonNull
-        public TimeHour_24Android[] newArray(int size) {
-            return new TimeHour_24Android[size];
+        public TimeHour24Android[] newArray(int size) {
+            return new TimeHour24Android[size];
         }
 
         /**
          * {@inheritDoc}
          */
         @NonNull
-        public TimeHour_24Android createFromByteArray(@NonNull byte[] values) {
+        public TimeHour24Android createFromByteArray(@NonNull byte[] values) {
             BluetoothGattCharacteristic bluetoothGattCharacteristic = new BluetoothGattCharacteristic(TIME_HOUR_24_CHARACTERISTIC, 0, 0);
             bluetoothGattCharacteristic.setValue(values);
-            return new TimeHour_24Android(bluetoothGattCharacteristic);
+            return new TimeHour24Android(bluetoothGattCharacteristic);
         }
 
     };
@@ -57,8 +56,17 @@ public class TimeHour_24Android extends TimeHour_24 implements Parcelable {
      *
      * @param bluetoothGattCharacteristic Characteristics UUID: 0x2B14
      */
-    public TimeHour_24Android(@NonNull BluetoothGattCharacteristic bluetoothGattCharacteristic) {
+    public TimeHour24Android(@NonNull BluetoothGattCharacteristic bluetoothGattCharacteristic) {
         super(bluetoothGattCharacteristic.getValue());
+    }
+
+    /**
+     * Constructor from parameters
+     *
+     * @param timeHour24 Time Hour 24
+     */
+    public TimeHour24Android(int timeHour24) {
+        super(timeHour24);
     }
 
     /**
@@ -66,7 +74,7 @@ public class TimeHour_24Android extends TimeHour_24 implements Parcelable {
      *
      * @param in Parcel
      */
-    private TimeHour_24Android(@NonNull Parcel in) {
+    private TimeHour24Android(@NonNull Parcel in) {
         //noinspection ConstantConditions
         super(in.createByteArray());
     }
